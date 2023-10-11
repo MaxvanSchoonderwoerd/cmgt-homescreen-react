7a1469006a6cf04f975c98fbf9fc01caf9837c3d
MaxvanSchoonderwoerd <maxvanschoonderwoerd@gmail.com>
2023-10-11 11:27:50 +0200

    added portfolio icon

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,4 +1,10 @@
 export default function FooterComponent() {
+  const iconStyle = {
+    fill: "none",
+    stroke: " #ffffff",
+    strokeLinecap: "round" as strokeLinecap,
+    strokeLinejoin: "round" as strokeLinejoin,
+  };
   return (
     <footer className="footer">
       <div className="container flex">
@@ -46,14 +52,34 @@ export default function FooterComponent() {
               <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path>
             </svg>
           </a>
+          <a className="icon" href="https://maxvanschoonderwoerd.netlify.app/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24px"
+              height="24px"
+              viewBox="0 0 48 48"
+              stroke="#ffffff"
+              strokeWidth="1.8"
+              transform="matrix(1, 0, 0, 1, 0, 0"
+            >
+              <path
+                className="a"
+                style={iconStyle}
+                d="M14,4.5H10.5a2,2,0,0,0-2,2v35a2,2,0,0,0,2,2h27a2,2,0,0,0,2-2V6.5a2,2,0,0,0-2-2H24"
+              />
+              <path
+                className="a"
+                style={iconStyle}
+                d="M12,4.5l1.4142-1.4142A2,2,0,0,1,14.8284,2.5H23a1,1,0,0,1,1,1v25l-5-5-5,5V4.5"
+              />
+              <line className="a" x1="14" y1="38" x2="34" y2="38" />
+              <line className="a" x1="24" y1="10" x2="34" y2="10" />
+              <line className="a" x1="24" y1="17" x2="34" y2="17" />
+              <line className="a" x1="24" y1="24" x2="34" y2="24" />
+              <line className="a" x1="14" y1="31" x2="34" y2="31" />
+            </svg>
+          </a>
         </div>
-
-        <a
-          className="portfolioLink"
-          href="https://maxvanschoonderwoerd.netlify.app/"
-        >
-          See my <span className="underlined">portfolio</span> for more work
-        </a>
       </div>
     </footer>
   );
